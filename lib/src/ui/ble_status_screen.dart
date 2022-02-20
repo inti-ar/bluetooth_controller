e2ba41261a89df51da43bdd8bfc71987afa8e34c
Luciano Nahuel Vitale <lucianonahuelvitale@gmail.com>
2022-02-20 19:35:43 -0300

    Implemented button to enable Bluetooth

--- lib/src/ui/ble_status_screen.dart
+++ lib/src/ui/ble_status_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
+import 'package:bluetooth_enable_fork/bluetooth_enable_fork.dart';
 
 class BleStatusScreen extends StatelessWidget {
   const BleStatusScreen({required this.status, Key? key}) : super(key: key);
@@ -23,10 +24,30 @@ class BleStatusScreen extends StatelessWidget {
     }
   }
 
+  Future<void> _enableBT() async {
+    await BluetoothEnable.enableBluetooth;
+  }
+
   @override
   Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Text('INTI Bluetooth Controller'),
+        ),
         body: Center(
-          child: Text(determineText(status)),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(determineText(status)),
+              status == BleStatus.poweredOff
+                  ? ElevatedButton(
+                      onPressed: (() {
+                        _enableBT();
+                      }),
+                      child: const Text('Turn on Bluetooth'),
+                    )
+                  : Container(),
+            ],
+          ),
         ),
       );
 }
