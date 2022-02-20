8c313b8b38ed2ef6f3a1bed69af07d112b08fc2b
Luciano Nahuel Vitale <lucianonahuelvitale@gmail.com>
2022-02-19 22:00:02 -0300

    Redesigned project with Flutter Reactive BLE

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,97 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
+import 'package:bluetooth_controller/src/ble/ble_device_connector.dart';
+import 'package:bluetooth_controller/src/ble/ble_device_interactor.dart';
+import 'package:bluetooth_controller/src/ble/ble_scanner.dart';
+import 'package:bluetooth_controller/src/ble/ble_status_monitor.dart';
+import 'package:bluetooth_controller/src/ui/ble_status_screen.dart';
+import 'package:bluetooth_controller/src/ui/device_list.dart';
+import 'package:provider/provider.dart';
 
-import 'main_page.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-void main() => runApp(const MyApp());
+import 'src/ble/ble_logger.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+const _themeColor = Colors.lightBlue;
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final _bleLogger = BleLogger();
+  final _ble = FlutterReactiveBle();
+  final _scanner = BleScanner(ble: _ble, logMessage: _bleLogger.addToLog);
+  final _monitor = BleStatusMonitor(_ble);
+  final _connector = BleDeviceConnector(
+    ble: _ble,
+    logMessage: _bleLogger.addToLog,
+  );
+  final _serviceDiscoverer = BleDeviceInteractor(
+    bleDiscoverServices: _ble.discoverServices,
+    readCharacteristic: _ble.readCharacteristic,
+    writeWithResponse: _ble.writeCharacteristicWithResponse,
+    writeWithOutResponse: _ble.writeCharacteristicWithoutResponse,
+    subscribeToCharacteristic: _ble.subscribeToCharacteristic,
+    logMessage: _bleLogger.addToLog,
+  );
+  runApp(
+    MultiProvider(
+      providers: [
+        Provider.value(value: _scanner),
+        Provider.value(value: _monitor),
+        Provider.value(value: _connector),
+        Provider.value(value: _serviceDiscoverer),
+        Provider.value(value: _bleLogger),
+        StreamProvider<BleScannerState?>(
+          create: (_) => _scanner.state,
+          initialData: const BleScannerState(
+            discoveredDevices: [],
+            scanIsInProgress: false,
+          ),
+        ),
+        StreamProvider<BleStatus?>(
+          create: (_) => _monitor.state,
+          initialData: BleStatus.unknown,
+        ),
+        StreamProvider<ConnectionStateUpdate>(
+          create: (_) => _connector.state,
+          initialData: const ConnectionStateUpdate(
+            deviceId: 'Unknown device',
+            connectionState: DeviceConnectionState.disconnected,
+            failure: null,
+          ),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'INTI Bluetooth Controller',
+        color: _themeColor,
+        theme: ThemeData(primarySwatch: _themeColor),
+        home: const HomeScreen(),
+      ),
+    ),
+  );
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({
+    Key? key,
+  }) : super(key: key);
+
+  Future<void> _requestPermissions() async {
+    await Permission.bluetooth.request().isGranted;
+    await Permission.bluetoothConnect.request().isGranted;
+    await Permission.bluetoothScan.request().isGranted;
+    await Permission.location.request().isGranted;
+  }
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(home: MainPage());
-  }
+  Widget build(BuildContext context) => Consumer<BleStatus?>(
+        builder: (_, status, __) {
+          if (status == BleStatus.ready) {
+            return const DeviceListScreen();
+          } else {
+            _requestPermissions();
+            return BleStatusScreen(status: status ?? BleStatus.unknown);
+          }
+        },
+      );
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,8 +10,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:bluetooth_controller/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Home screen test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const HomeScreen());
   });
 }
